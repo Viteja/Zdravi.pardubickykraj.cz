89bfe2664f42ae5d2be932a43b1ec75b3f2c570a
Viteja <vitejajan@gmail.com>
2024-09-06 17:11:30 +0200

    commit

--- src/pages/obcan-content/zivot.jsx
+++ src/pages/obcan-content/zivot.jsx
@@ -13,20 +13,14 @@ export default function Zivot() {
     <>
       <Helmet>
         <meta charSet="utf-8" />
-        <title>
-          Jak si poradit v životních situacích | Zdraví Pardubického kraje
-        </title>
+        <title>Jak si poradit v životních situacích | Zdraví Pardubického kraje</title>
       </Helmet>
       <Navbar />
       <Chatbot />
       <Btnup />
-      <div
-        className="main-sec"
-        id="zivotni-situacich">
+      <div className="main-sec" id="zivotni-situacich">
         <RozcestnikObcan />
-        <div
-          className="semi-title"
-          id="semi">
+        <div className="semi-title" id="semi">
           <div className="container">
             <h2>Jak si poradit v životních situacích</h2>
           </div>
@@ -34,29 +28,12 @@ export default function Zivot() {
         <div className="container">
           <div className="semi-content">
             <p>
-              Moji milý spoluobčané,
-              <br /> zdravotnictví nás provází životem od našeho narození až po
-              ukončení života. V průběhu let se setkáváme s mnoha situacemi,
-              které se týkají zdraví nejen nás samotných, ale i našich blízkých
-              a známých. Proto je vždy dobré mít všeobecný přehled o tom, co
-              všechno nám může pomoct. V této brožuře najdete nejen základní
-              rady, jak se zachovat a kam se obrátit, ale také podrobný adresář
-              nejpotřebnějších zdravotnických a sociálních služeb. Nechybí ani
-              pohotovosti, paliativní péče nebo kontakty na další pomáhající a
-              spolupracující organizace v různých životních situacích. Přeji
-              vám, abyste tuto knihu potřebovali co nejméně, ale pokud k tomu
-              již dojde, aby vám byla vždy přínosem. S úctou Ing. Michaela
-              Matoušková, MPA, MHA náměstkyně hejtmana zodpovědná za
-              zdravotnictví.
+              Moji milí spoluobčané,
+              <br /> zdravotnictví nás provází životem od našeho narození až po ukončení života. V průběhu let se setkáváme s mnoha situacemi, které se týkají zdraví nejen nás samotných, ale i našich blízkých a známých. Proto je vždy dobré mít všeobecný přehled o tom, co všechno nám může pomoct. V této brožuře najdete nejen základní rady, jak se zachovat a kam se obrátit, ale také podrobný adresář nejpotřebnějších zdravotnických a sociálních služeb. Nechybí ani pohotovosti, paliativní péče nebo kontakty na další pomáhající a spolupracující organizace v různých životních situacích. Přeji vám, abyste tuto knihu potřebovali co nejméně, ale pokud k tomu již dojde, aby vám byla vždy přínosem. S úctou Ing. Michaela Matoušková, MPA, MHA náměstkyně hejtmana zodpovědná za zdravotnictví.
             </p>
             <p>
-              <a
-                href="/Jak_si_poradit_v_zivotnich_situacich.pdf"
-                target="_blank"
-                className="link">
-                <span data-content="Jak si poradit v životních situacích">
-                  Jak si poradit v životních situacích
-                </span>
+              <a href="/Jak_si_poradit_v_zivotnich_situacich.pdf" target="_blank" className="link">
+                <span data-content="Jak si poradit v životních situacích">Jak si poradit v životních situacích</span>
               </a>
             </p>
           </div>
